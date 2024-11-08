e9c69ec933bb392b2f875591061367c0c80189dc
Mariana <marianagutierrezvivi@gmail.com>
2024-11-08 10:40:36 +0300

    Create index.js

---
+++ views/index.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    let chat = document.querySelector('div.container-fluid');
+    
+    function addMsg(obj) {
+        let html = document.createElement('span');
+        html.className = 'msg';
+        html.innerHTML = obj;
+        chat.appendChild(html);
+    }
+    
+    window.onclick = () => {
+        let timestamp = new Date().toLocaleTimeString();
+        addMsg(`Message at ${timestamp}`);
+    }
+}
