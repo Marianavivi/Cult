fa3880fa0bc95a5cba49aafb234e2b103e7da401
Mariana <marianagutierrezvivi@gmail.com>
2024-11-08 12:01:19 +0300

    Create New.js

---
+++ plugins/New.js
@@ -0,0 +1,23 @@
+let handler = async m => {
+  m.reply(
+    `𝙷𝙴𝙻𝙻𝙾! 𝙸𝚏 𝚈𝚘𝚞 𝙽𝚎𝚎𝚍 𝙷𝚎𝚕𝚙 𝚆𝚒𝚝𝚑 𝙰𝚗𝚢 𝚘𝚏 𝚃𝚑𝚎 𝙰𝚋𝚘𝚟𝚎, 𝙿𝚕𝚎𝚊𝚜𝚎 𝙵𝚘𝚕𝚕𝚘𝚠 𝚃𝚑𝚎𝚜𝚎 𝙸𝚗𝚜𝚝𝚛𝚞𝚌𝚝𝚒𝚘𝚗𝚜:
+
+1. **Join This Channel First:**
+   [WhatsApp Channel](https://whatsapp.com/channel/0029VaLYCPXJENxtW7BU9a0u)
+   
+   Once you've joined the channel, my owner will assist you.
+
+2. **If This Is Not The Real Owner, Contact The Developer:**
+   - WhatsApp: *+254732647560*
+   - © Mariana Gutierrez
+
+**Note:** Make sure to describe your issue clearly so we can assist you better.
+`.trim()
+  );
+};
+
+handler.help = ['ruth'];
+handler.tags = ['main'];
+handler.command = ['mrcs'];
+
+export default handler;
